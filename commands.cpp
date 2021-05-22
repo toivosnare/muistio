bd516abf0700ce3279a3b0612fb0aa6bd1864594
Toivo Snåre <topisn@gmail.com>
2021-05-22 17:49:13 +0300

    Don't transfer command line arguments to processes spawned by the NewWindow command

--- commands.cpp
+++ commands.cpp
@@ -40,10 +40,15 @@ VOID New(HWND hWnd) {
 }
 
 VOID NewWindow() {
-    STARTUPINFOW si{};
-    si.cb = sizeof(si);
-    PROCESS_INFORMATION pi{};
-    CreateProcessW(NULL, GetCommandLineW(), NULL, NULL, FALSE, NULL, NULL, NULL, &si, &pi);
+    CONST LPWSTR commandLine = GetCommandLineW();
+    INT argc;
+    CONST LPWSTR *argv = CommandLineToArgvW(commandLine, &argc);
+    if (argc > 0) {
+        STARTUPINFOW si{};
+        si.cb = sizeof(si);
+        PROCESS_INFORMATION pi{};
+        CreateProcessW(argv[0], NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &si, &pi);
+    }
 }
 
 static UINT_PTR CALLBACK OpenProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
@@ -380,17 +385,15 @@ VOID Init(HWND hWnd) {
     CONST LPWSTR commandLine = GetCommandLineW();
     INT argc;
     CONST LPWSTR *argv = CommandLineToArgvW(commandLine, &argc);
-    if (argc > 2) {
-        STARTUPINFOW si{};
-        si.cb = sizeof(si);
-        PROCESS_INFORMATION pi{};
-        for (INT i = 2; i < argc; ++i) {
-            CONST UINT SIZE = wcslen(argv[0]) + wcslen(argv[i]) + 2;
-            LPWSTR command = new WCHAR[SIZE];
-            swprintf_s(command, SIZE, L"%s %s", argv[0], argv[i]);
-            CreateProcessW(NULL, command, NULL, NULL, FALSE, NULL, NULL, NULL, &si, &pi);
-            delete[] command;
-        }
+    STARTUPINFOW si{};
+    si.cb = sizeof(si);
+    PROCESS_INFORMATION pi{};
+    for (INT i = 2; i < argc; ++i) {
+        CONST UINT SIZE = wcslen(argv[0]) + wcslen(argv[i]) + 2;
+        LPWSTR command = new WCHAR[SIZE];
+        swprintf_s(command, SIZE, L"%s %s", argv[0], argv[i]);
+        CreateProcessW(NULL, command, NULL, NULL, FALSE, NULL, NULL, NULL, &si, &pi);
+        delete[] command;
     }
     if (argc > 1 && Read(hWnd, argv[1], encoding)) {
         wcscpy_s(openFile, MAX_PATH, argv[1]);
